2e0de523f917d91dd774130e5c783ff61daf61b6
vmeelo <vinicius.melo@dcomp.ufs.br>
2022-08-11 23:32:17 -0300

    feat: deploy

--- src/connection.ts
+++ src/connection.ts
@@ -6,13 +6,11 @@ import "mysql2";
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: 'us-cdbr-east-06.cleardb.net',
-    port: parseInt(process.env.DB_PORT || '3306'),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     entities: [__dirname + "/model/*.{js,ts}"],
 });
-console.log('*****'+process.env.DB_LOCAL_HOST)
 AppDataSource.initialize() 
     .then(() => { 
         console.log("Data Source has been initialized!")
