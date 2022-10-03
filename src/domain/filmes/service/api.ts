09b415d5911da936a5837817ae8b83a3977f34a6
Vinicius <vstmelo@proton.me>
2022-10-03 19:12:41 -0300

    deploy

--- src/domain/filmes/service/api.ts
+++ src/domain/filmes/service/api.ts
@@ -2,5 +2,11 @@ import axios, { AxiosInstance } from "axios";
 import 'dotenv/config';
 
 export const api : AxiosInstance  = axios.create({
-    baseURL:  "http://ghibliapi.herokuapp.com"
+    baseURL: process.env.ENVIROMENT === 'local'
+        ? process.env.DB_HOST_LOCAL
+        : process.env.DB_HEROKU
+});
+
+export const apiHeroku: AxiosInstance = axios.create({
+    baseURL: process.env.DB_HEROKU
 });
